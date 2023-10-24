7232c2694b0c47017ebeae784dcefa706b3ad75a
metchmedina <117733082+metchmedina@users.noreply.github.com>
2023-10-24 17:25:48 +0800

    Add files via upload

---
+++ PRELIM/FavoriteSong.java
@@ -0,0 +1,15 @@
+public class FavoriteSong  {
+   public static void main(String[] args) {
+      String FirstSong  = "CIRCLE | BY:Post Malone";
+      String SecondSong = "MONEY TREES | BY:Kendrick Lamar";
+      String ThirdSong  = "TELEVISION | By:Rex Orange County";
+      String FourthSong = "CASTLE ON THE HILL | By: Ed Sheeran";
+      
+      System.out.println("THESE ARE MY FAVORITE SONGS:");   
+      System.out.println("1.) " +FirstSong);
+      System.out.println("2.) " +SecondSong);
+      System.out.println("3.) " +ThirdSong);
+      System.out.println("4.) " +FourthSong);
+      
+  }
+ }
